a3817a34a2711edca842ccaa3cd0711f10db03b5
Dmitry Ignatyev <dmitry.ignatyev@piecestech.com>
2023-02-01 15:02:37 +0100

    query service docker config

--- query-service/src/main/java/com/di/twitter/analytics/query/service/api/DocumentController.java
+++ query-service/src/main/java/com/di/twitter/analytics/query/service/api/DocumentController.java
@@ -3,6 +3,9 @@ package com.di.twitter.analytics.query.service.api;
 import com.di.twitter.analytics.query.service.model.QueryServiceRequestModel;
 import com.di.twitter.analytics.query.service.model.QueryServiceResponseModel;
 import com.di.twitter.analytics.query.service.service.ElasticQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -24,12 +27,25 @@ public class DocumentController {
 
     private final ElasticQueryService queryService;
 
+    @Operation(summary = "Get all documents")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "Not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping("/")
     public ResponseEntity<List<QueryServiceResponseModel>> getAllDocuments() {
         List<QueryServiceResponseModel> modelList = queryService.getAllDocuments();
         log.info("Returning {} documents", modelList.size());
         return ResponseEntity.ok(modelList);
     }
+
+    @Operation(summary = "Get the document by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "Not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
 
     @GetMapping("/{id}")
     public ResponseEntity<QueryServiceResponseModel> getDocumentbyId(@PathVariable @NotEmpty String id) {
@@ -38,6 +54,12 @@ public class DocumentController {
         return ResponseEntity.ok(responseModel);
     }
 
+    @Operation(summary = "Searching a document by text")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "Not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping("/text")
     ResponseEntity<List<QueryServiceResponseModel>> getDocumentsByText(@Valid
             @RequestBody QueryServiceRequestModel requestModel) {
--- query-service/src/main/java/com/di/twitter/analytics/query/service/config/WebSecurityConfig.java
+++ query-service/src/main/java/com/di/twitter/analytics/query/service/config/WebSecurityConfig.java
@@ -1,7 +1,10 @@
 package com.di.twitter.analytics.query.service.config;
 
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,8 +12,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${security.paths-to-ignore")
+    private String[] pathsToIgnore;
+
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests().antMatchers("/**").permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity webSecurity) {
+        webSecurity.ignoring()
+                .antMatchers(pathsToIgnore);
     }
 }
